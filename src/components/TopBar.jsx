aee54dfef2d4553357617d533672651d0567e113
Nicolás Alceo Davite <nicolasalceodavite@gmail.com>
2022-10-04 19:41:19 -0300

    divide ContentRowTop in three components

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -24,7 +24,11 @@ const TopBar = () => {
 
         {/* <!-- Nav Item - Messages --> */}
         <li className="nav-item dropdown no-arrow mx-1">
-          <a className="nav-link dropdown-toggle" href="/" id="messagesDropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="/"
+            id="messagesDropdown"
+          >
             <i className="fas fa-envelope fa-fw"></i>
             {/* <!-- Counter - Messages --> */}
             <span className="badge badge-danger badge-counter">7</span>
